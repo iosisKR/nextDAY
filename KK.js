c74dac44d04844f5e8b86168f85785ca6ae0c49c
iosis <kksscc0913@gmail.com>
2022-11-09 21:59:38 +0900

    first

---
+++ KK.js
@@ -0,0 +1,23 @@
+var Target = document.getElementById("clock");
+        function clock() {
+            var time = new Date();
+
+            var month = time.getMonth();
+            var date = time.getDate();
+            var day = time.getDay();
+            var week = ['일', '월', '화', '수', '목', '금', '토'];
+
+            var hours = time.getHours();
+            var minutes = time.getMinutes();
+            var seconds = time.getSeconds();
+
+            Target.innerText = 
+            `${month + 1}월 ${date}일 ${week[day]}요일 ` +
+            `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+        }
+
+
+var Target = document.getElementById("clock");
+Target.addEventListener('click', function () {
+  alert('Hello world');
+})
